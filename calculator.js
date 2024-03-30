ab039713fb79818de2d055f4259b3506d97a939c
Onyiriuka Justin <richardjustin500@gmail.com>
2024-03-30 11:53:48 +0100

    improved functionality

--- calculator.js
+++ calculator.js
@@ -26,27 +26,55 @@ function calculation() {
 
 
 function changeName() {
-    const newNameInput = document.getElementById("newName").value.trim();
-    const oldName = document.getElementById("currentCalculatorName").textContent;
-
-    
-
+    const newNameInput = document.getElementById("newName");
+    const oldName = document.getElementById("currentCalculatorName");
 
     if (newNameInput === "") {
         alert("input a valid name");
-    }else{
-        let display = confirm("Set new name to " + newNameInput + "?");
+    } else {
+        let display = confirm("Set new name to " + newNameInput.value + "?");
+
         if (display === true) {
-            document.getElementById("currentCalculatorName").textContent = newNameInput + "'s Calculator";
+            oldName.innerHTML = `${newNameInput.value}'s Calculator`;
         }
-        
-    }    
-
-
-
-
+    }
     let CurrentFirstLetterValue = document.getElementById("CurrentFirstLetterValue");
     const newFirstLetterValue = document.getElementById("newName").value[0].toUpperCase()
-    CurrentFirstLetterValue.value = newFirstLetterValue + "C";
-    
+    CurrentFirstLetterValue.value = `${newFirstLetterValue}C`;
+
 }
+
+
+// digitalClock 
+
+function digitalClock() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    let clockMinutes = document.querySelector('.minutes');
+    let clockHours = document.querySelector('.hours');
+    let clockSeconds = document.querySelector(".seconds")
+
+
+    if (clockHours.textContent < 9) {
+        clockHours.textContent = `0${hours}`;
+    } else {
+        clockHours.textContent = `${hours}:`
+    }
+
+    if (clockMinutes.innerHTML < 9) {
+        clockMinutes.innerHTML = `0${minutes}`;
+    } else {
+        clockMinutes.innerHTML = `${minutes}:`
+    }
+
+    if (clockSeconds.innerHTML < 9) {
+        clockSeconds.innerHTML = `0${seconds}`
+    } else {
+        clockSeconds.innerHTML = `${seconds}`
+    }
+}
+
+setInterval(digitalClock, 1000)
